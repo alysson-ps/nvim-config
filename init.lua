64efc9926914e177e9872ee6c052fe88292c886d
Alisson porto <dev.alysson@gmail.com>
2023-12-27 21:01:22 -0300

    chore: minimal config

---
+++ init.lua
@@ -0,0 +1,17 @@
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+require("config.options")
+require("config.keymaps")
+require("config.lazy")
+require("config.autocmds")
