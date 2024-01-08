835c323c8d78fae844ca97d96b7a45146338e42a
Alisson porto <dev.alysson@gmail.com>
2024-01-07 22:47:33 -0300

    refactor: set config bufferline external

---
+++ lua/plugins/configs/bufferline.lua
@@ -0,0 +1,38 @@
+return function(_, _)
+  local bufferline = require("bufferline")
+
+  local setup = {
+    options = {
+      -- style_preset = ,
+      -- stylua: ignore
+      close_command = function(n) require("mini.bufremove").delete(n, false) end,
+      -- stylua: ignore
+      right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = true,
+      diagnostics_indicator = function(_, _, diag)
+        local icons = require("config.icons.nerd_font")
+        local ret = (diag.error and icons.DiagnosticError or "") .. (diag.warning and icons.DiagnosticWarn or "")
+        return vim.trim(ret)
+      end,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
+    },
+  }
+
+  bufferline.setup(setup)
+  -- Fix bufferline when restoring a session
+  vim.api.nvim_create_autocmd("BufAdd", {
+    callback = function()
+      vim.schedule(function()
+        pcall(nvim_bufferline)
+      end)
+    end,
+  })
+end
